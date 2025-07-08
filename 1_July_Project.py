65d6ad93acbe8210113d4db4889e45c9f1894b5b
Aditya-DS-AI <115344775+Aditya-DS-AI@users.noreply.github.com>
2025-07-08 23:01:14 +0530

    Add files via upload

--- 1_July_Project.py
+++ 1_July_Project.py
@@ -1,23 +1,27 @@
 import streamlit as st
 import joblib
-import pandas as pd
-import gmail
+import pandas as pd	
+import requests
+
 
 spam_model=joblib.load("spam_classifier.pkl")
 language_model=joblib.load("lang_det.pkl")
 news_model=joblib.load("news_cat.pkl")
 review_model=joblib.load("review.pkl")
+df=joblib.load("df.pkl")
+vectors=joblib.load("vectors.pkl")
+model=joblib.load("mv_rcmd.pkl")
 
 st.set_page_config(layout="wide")
 
 
 st.markdown("""
     <h1 style='background-color: powderblue; color: black; padding: 10px; border-radius: 10px; text-align: center;'>
-        🎯 LENS eXpert(NLP Suite)
+        🎯 LENSR eXpert(NLP Suite)
     </h1>
 """, unsafe_allow_html=True)
 st.title("")
-tab1,tab2,tab3,tab4=st.tabs(["🤖 Spam Classifier","🗣️ Language Detection","👍 Food Review Sentiment 👎","📰 News Classification"])
+tab1,tab2,tab3,tab4,tab5=st.tabs(["🤖 Spam Classifier","🗣️ Language Detection","👍 Food Review Sentiment 👎","🎬 Movie Recommemndation","📰 News Classification"])
 with tab1:
     msg=st.text_input("Enter Msg")
     if st.button("Prediction"):
@@ -80,6 +84,28 @@ with tab3:
         df["Prediction"]=df["Prediction"].map({0:'Disliked',1:'Liked'})
         st.dataframe(df)
 with tab4:
+	selected_name = st.selectbox("", ["🎬 Choose a movie"] +sorted(df['name']))
+	if selected_name!= "🎬 Choose a movie":
+            index=df[df.name==selected_name].index[0]
+            distances,indexes=model.kneighbors([vectors[index]],n_neighbors=6)
+            cols=st.columns(5)
+            j=0
+            for i in indexes[0][1:]:
+                mid=df.loc[i]['movie_id']
+                api_url=f"http://www.omdbapi.com/?i={mid}&apikey=48972038"
+                resp=requests.get(api_url)
+                poster_url=resp.json()['Poster']
+                col = cols[j]
+                j+=1
+                with col:
+                    st.write(df.loc[i]['name'])
+                    try:
+                        st.image(poster_url)
+                        print(poster_url)
+                    except:
+                        st.image("poster_na.jpeg")
+                    
+with tab5:
     st.image("under_const.jpg")
 st.sidebar.image("flag.jpg")
 
